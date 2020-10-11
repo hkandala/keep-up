8751174010b13ef0dafb58cbd1b0b947c93011b3
Kandala, Harish <hakandal@visa.com>
2020-10-11 17:58:43 +0530

    Add dark theme toggle to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,32 @@
-import { GeistProvider, CssBaseline } from "@geist-ui/react";
+import { useState } from "react";
+import { GeistProvider, CssBaseline, Toggle } from "@geist-ui/react";
+import { Moon, Sun } from "@geist-ui/react-icons";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const defaultTheme = "light";
+  const [themeType, setThemeType] = useState(defaultTheme);
+  const switchThemes = () => {
+    setThemeType((lastThemeType) =>
+      lastThemeType === "dark" ? "light" : "dark"
+    );
+  };
+
   return (
-    <GeistProvider>
+    <GeistProvider theme={{ type: themeType }}>
       <CssBaseline />
+
+      <div style={{ marginTop: 25, marginRight: 25, textAlign: "right" }}>
+        <Sun />
+        <Toggle
+          onChange={switchThemes}
+          initialChecked={defaultTheme == "dark"}
+          style={{ marginLeft: 10, marginRight: 10 }}
+        />
+        <Moon />
+      </div>
+
       <Component {...pageProps} />
     </GeistProvider>
   );
