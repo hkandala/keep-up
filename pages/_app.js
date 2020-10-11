00b72c9a0720066af9700adc85d27275dcc49825
Kandala, Harish <hakandal@visa.com>
2020-10-11 16:44:02 +0530

    Integrate geist-ui instead of antd

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { GeistProvider, CssBaseline } from "@geist-ui/react";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </GeistProvider>
+  );
 }
 
 export default MyApp;
